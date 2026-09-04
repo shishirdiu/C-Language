07a0a1ee004e3d7f202bf577d09cf1de61b0fd88
Shishir Sarker <shishirsarker78@gmail.com>
2026-09-04 11:31:38 +0600

    Add typecasting examples in typecasting.c
    Demonstrate implicit and explicit typecasting in C.

---
+++ typecasting.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+
+int main(){
+	int shi=20;
+	float apon=shi;//implicit typecasting
+	printf("%f",apon);
+	return 0;
+}
+
+#include<stdio.h>
+
+
+int main(){
+float shi = 199.9999;
+int apon =(int)shi; //explicit typecasting
+printf("%d",apon);
+	return 0;
+}
